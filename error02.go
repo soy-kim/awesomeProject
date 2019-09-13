3575634358e96e7bc9410208d108eaab80fb35d5
soy-kim <soy.kim0703@gmail.com>
2019-09-13 16:55:18 +0900

    commit

---
+++ error02.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func helloOne(n int) (string, error) {
+	if n == 1 {
+		s := fmt.Sprintln("Hello", n)
+		return s, nil
+	}
+
+	return "", fmt.Errorf("%d is not 1.", n)
+}
+
+func main() {
+	s, err := helloOne(1)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(s)
+
+	s, err = helloOne(2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(s)
+
+	fmt.Println("Hello, world!")
+}
